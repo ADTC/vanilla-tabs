bc57ad7bc278fc09a1a8e3423d2c38df442447bb
Dmytro Kudleichuk <d.kudleichuk@gmail.com>
2019-07-27 14:09:20 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # vanilla-tabs
 Responsive tabs-to-accordion script without jQuery, written using pure JavaScript
+
+<p>
+<img src="https://img.shields.io/badge/dependencies-no-success.svg" alt="Dependencies" />
+<img src="https://img.shields.io/badge/license-MIT-green.svg" alt="License" />
+</p>
+
+## Getting Started
+
+Load stylesheet and script file
+
+```
+<html>
+<head>
+	...
+	<link rel="stylesheet" type="text/css" href="path/to/vanilla.tabs.css">
+</head>
+<body>
+	...
+	<!--
+		Before closing </body>
+	-->
+	<script src="path/to/vanilla.tabs.js"></script>
+	<script>
+		document.addEventListener('DOMContentLoaded', () => {
+			new VanillaTabs({
+				'selector': '.tabs',		// default is ".tabs"
+				'type': 'horizontal',		// can be horizontal / vertical / accordion
+				'responsiveBreak': 840,	// tabs become accordion on this device width
+				'activeIndex' : 0				// active tab index (starts from 0 )
+			});
+		});
+	</script>
+</body>
+</html>
+```
+
+## Markup 
+
+```
+<ul class="tabs">
+	<li data-title="First tab" class="tabs__content">Put here any content for a first tab</li>
+	<li data-title="Second tab" class="tabs__content">Put here any content for a second tab</li>
+	<li data-title="Third tab" class="tabs__content">Put here any content for a third tab</li>
+</ul>
+```
+
+## License
+
+Feel free to use it on your projects without any restrictions
---
+++ dist/vanilla.tabs.js
@@ -0,0 +1,179 @@
+'use strict';
+
+/**
+ * 
+ * Hey there! Here is my *really first* plugin, written using Pure JavaScript / ES5
+ * Hope you'll enjoy using it!
+ * 
+ * ToDo: Re-write a plugin using ES6
+ * 
+ */
+
+/**
+  * Start writing a plugin using Immediately Invoked Function Expression (IIFE).
+  * Function is used to create a private scope: variable, created inside a function
+  * is not accessible outside the function
+ */
+
+(function(){
+
+	// Constructor function
+	const VanillaTabs = function( opts ) {
+
+		this.options = Object.assign( VanillaTabs.defaults, opts );
+		this.elems = document.querySelectorAll( this.options.selector );
+
+		buildUI( this );
+		handleNavigation( this );
+		handleResponsive( this );
+
+	}
+
+	// Private function to initialize the UI Elements
+	function buildUI( tabs ){
+
+		// walk on all tabs on the page
+		tabs.elems.forEach( function( el, i ) {
+
+			let tabsElem = el,
+			childNodes = tabsElem.childNodes,
+			tabsTitles = [];
+
+			tabsElem.classList.add( 'style__' + tabs.options.type );
+
+			for( let i = 0; i < childNodes.length; i++ ) {
+
+				if( childNodes[i].classList && childNodes[i].classList.contains( 'tabs__content') ) {
+
+					let tabItem = childNodes[i];
+
+					// grab tab title from data attribute
+					let tabTitle = tabItem.dataset.title ? tabItem.dataset.title : '';
+					tabsTitles.push( tabTitle );
+
+					// wrap tab content
+					let tabContent = tabItem.innerHTML;
+					tabItem.innerHTML = '<div class="tabs__content_wrapper">' + tabContent + '</div>';
+
+					// insert nav link for accordion navigation
+					tabItem.insertAdjacentHTML( 'afterbegin', '<a class="tabs__nav_link">' + tabTitle + '</a>');
+
+				}
+
+			}
+
+			// create horizontal / vertical tabs navigation elements
+			let navElemsHTML = '';
+
+			tabsTitles.forEach( function( title ) {
+				navElemsHTML = navElemsHTML + '<a class="tabs__nav_link">' + title + '</a>';
+			});
+
+			tabsElem.insertAdjacentHTML( 'afterbegin', '<li class="tabs__nav">' + navElemsHTML + '</li>');
+
+			// set initial active tab
+			let activeTabIndex = Number( tabs.options.activeIndex );
+			tabsElem.querySelectorAll( '.tabs__nav > .tabs__nav_link')[ activeTabIndex ].classList.add( 'is__active' );
+			tabsElem.querySelectorAll( '.tabs__content')[ activeTabIndex ].classList.add( 'is__active' );
+			tabsElem.querySelectorAll( '.tabs__content > .tabs__nav_link')[ activeTabIndex ].classList.add( 'is__active' );
+
+		});
+
+	}
+
+	// Navigation: assign click events
+	function handleNavigation( tabs ) {
+
+		// walk on all tabs on the page
+		tabs.elems.forEach( function( el, i ) {
+
+			let tabsElem = el;
+
+			tabsElem.addEventListener( 'click', function( e ){
+
+				if( e.target && e.target.classList.contains( 'tabs__nav_link') ) {
+					e.preventDefault();
+
+					let activeTabIndex;
+
+					// if we click on main navigation link
+					if( e.target.parentElement.classList == 'tabs__nav' ) {
+						activeTabIndex = Array.prototype.slice.call( e.target.parentElement.children ).indexOf( e.target );
+
+					// if we click on accordion nav link
+					} else {
+						activeTabIndex = Array.prototype.slice.call( e.target.parentElement.parentElement.children ).indexOf( e.target.parentElement ) - 1;
+					}
+
+					let tabsContent = tabsElem.getElementsByClassName( 'tabs__content');
+
+					// remove active class for inactive tabs
+					for( let i = 0; i < tabsContent.length; i++ ) {
+						tabsContent[ i ].classList.remove( 'is__active');
+					}
+
+					// add active class for a current (active) tab
+					tabsContent[ activeTabIndex ].classList.add( 'is__active');
+
+					// add active classes and remove inactive for main nav links
+					let mainNavLinks = tabsElem.querySelectorAll( '.tabs__nav > .tabs__nav_link');
+					mainNavLinks.forEach( function( el ) {
+						el.classList.remove( 'is__active');
+					});
+					mainNavLinks[ activeTabIndex ].classList.add( 'is__active');
+
+					// add active classes and remove inactive for accordion nav links
+					let accordionNavLinks = tabsElem.querySelectorAll( '.tabs__content > .tabs__nav_link');
+					accordionNavLinks.forEach( function( el ) {
+						el.classList.remove( 'is__active');
+					});
+					accordionNavLinks[ activeTabIndex ].classList.add( 'is__active');
+
+				}
+
+			});
+
+		});
+
+	}
+
+	// Responsive: tabs to accordion
+	function handleResponsive( tabs ) {
+
+		const responsiveClassName = 'is__responsive';
+
+		window.addEventListener( 'resize', function() {
+
+			// walk on all tabs on the page
+			tabs.elems.forEach( function( el, i ) {
+
+				if( window.innerWidth > Number( tabs.options.responsiveBreak ) ) {
+
+					el.classList.remove( responsiveClassName );
+
+				} else {
+
+					el.classList.add( responsiveClassName );
+
+				}
+
+			});
+
+		});
+
+		// manually fire resize event
+		window.dispatchEvent( new Event( 'resize' ));
+
+	}
+
+	// Attach our defaults for plugin to the plugin itself
+	VanillaTabs.defaults = {
+		'selector': '.tabs',
+		'type': 'horizontal',
+		'responsiveBreak': 840
+	}
+
+	// make accessible globally
+	window.VanillaTabs = VanillaTabs;
+
+})();
